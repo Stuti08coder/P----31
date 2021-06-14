96da3c199df2d0e97d7d3a9f2cbb827f3ecb344f
Stuti08coder <76516771+Stuti08coder@users.noreply.github.com>
2021-06-14 09:41:54 +0530

    Add files via upload

---
+++ Particle.js
@@ -0,0 +1,17 @@
+class Particle{
+    constructor(x,y,) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.circle(x,y,ths.r,options);
+     this.color = color(random(0,255), random(0,255), random(0,255))
+      World.add(world, this.body);
+    }
+    display(){
+    FileList("yellow")
+    pop()
+    push()
+ 
+  
+    }
+  }
